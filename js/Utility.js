481a776af60a835b880536ac84b847dc6306f242
mlWebDeveloper <mlwebdeveloper@outlook.com>
2023-12-26 00:01:57 -0500

    Add setAttributes to Utility
    Clean up code

--- js/Utility.js
+++ js/Utility.js
@@ -17,6 +17,12 @@ export default class Utility {
         });
     }
 
+    setAttributes(element, attributes) {
+        for (const key in attributes) {
+            element.setAttribute(key, attributes[key]);
+        }
+    }
+
     async loadHtmlTemplate(url, targetElementId) {
         try {
             const response = await fetch(url);
@@ -30,43 +36,3 @@ export default class Utility {
     }
 }
 
-
-
-
-
-/*export default class Utility {
-    constructor() {}
-
-    querySelector(selector) {
-        return document.querySelector(selector);
-    }
-
-    createElements(elements) {
-        return elements.map(({ tagName, content = '', attributes = {} }) => {
-            const element = document.createElement(tagName);
-            if (content) element.textContent = content;
-            this.setAttributes(element, attributes);
-            return element;
-        });
-    }
-
-    setAttributes(element, attributes) {
-        for (const key in attributes) {
-            element.setAttribute(key, attributes[key]);
-        }
-    }
-
-    async loadHtmlTemplate(url, targetElementId) {
-        try {
-            const response = await fetch(url);
-            if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-            const html = await response.text();
-            const targetElement = this.querySelector(targetElementId);
-            if (targetElement) targetElement.innerHTML = html;
-        } catch (e) {
-            console.error("Error loading template:", e);
-            // Optionally update the UI to reflect the error
-        }
-    }
-}
-*/
